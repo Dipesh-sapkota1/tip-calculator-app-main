6a52b570271e1c865c55702dd0d71ec034b10a8c
Dipesh-sapkota1 <sapkotadipes.com>
2024-07-24 16:54:11 +0545

    refactored

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-const billAmt = document.getElementById('bill-amt').value;
-const people = document.getElementById('people').value;
+const tipBtn = document.querySelectorAll('.tip-btn');
+const tipbox = document.querySelectorAll('.radio-group');
 
-console.log(billAmt,people);
+// tipBtn.forEach(btn=>{
+//     btn.addEventListener('click',(e)=>{
+//         console.log(e.target.value)
+//     })
+// })
+ 
